7f6335be31799165a59318817a9c0d6c6135ae9e
Santiago Vallejos <vallejossantiago1412@gmail.com>
2025-09-10 17:28:47 -0300

    Add new images for contacts: 504081956 and devlights logo

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <footer className="w-full h-24 flex items-center justify-center border-t dark:border-gray-700 mt-8">
-            <p className="text-sm text-gray-500">© 2024 Sofía Cubilla. All rights reserved.</p>
+            <p className="text-sm text-gray-500">© 2025 Vallejos Santiago.</p>
         </footer>
     )
 }

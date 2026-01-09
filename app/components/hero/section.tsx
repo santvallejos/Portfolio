29ffa3cbdc5a15a4d619443de81a97be86107c77
Santiago Vallejos <vallejossantiago1412@gmail.com>
2026-01-09 13:23:38 -0300

    Fix: Update job title from Full Stack Developer to Software Developer

--- app/components/hero/section.tsx
+++ app/components/hero/section.tsx
@@ -86,7 +86,7 @@ export default function Hero() {
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.8, delay: 0.2 }}
                 >
-                    Full Stack Developer
+                    Software Developer
                 </motion.p>
 
                 {/* Social Icons Dock */}
